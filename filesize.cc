c0938a9bf37e4985739df92d526cf5f37b054896
AlexHeffner <alexjheffner@gmail.com>
2022-01-12 17:28:24 -0500

    First test

--- filesize.cc
+++ filesize.cc
@@ -8,11 +8,18 @@ using namespace std;
 // input.3lines: 3
 // input.1line: 1
 
+    // just to get you started, this is how to refer to the arguments that were passed
 int main( int argc, char* argv[] )
 {
-    // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-
+    for (int arg = 1; arg < argc; ++arg){
+        ifstream inFile("file"); 
+        inFile.open(argv[arg]);
+        if(inFile.fail()){
+            std::cout << argv[arg] << ": " << -1 << '\n';
+        }else{
+            int lineCount = count(istreambuf_iterator<char>(inFile), istreambuf_iterator<char>(), '\n');
+            std::cout << argv[arg] << ": " << lineCount << '\n' ;
+        }
+    }
     exit(0); // this means that the program executed correctly!
 }
