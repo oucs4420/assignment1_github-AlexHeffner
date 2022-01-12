85887902f27fbc341c69f0a7d01ad0c116a64fba
AlexHeffner <alexjheffner@gmail.com>
2022-01-12 17:41:42 -0500

    Passing test

--- filesize.cc
+++ filesize.cc
@@ -11,17 +11,18 @@ using namespace std;
     // just to get you started, this is how to refer to the arguments that were passed
 int main( int argc, char* argv[] )
 {
+    std::cout << "program: ./filesize" << "\n";
     for (int arg = 1; arg < argc; ++arg){
         std::ifstream inFile("file"); 
         inFile.open(argv[arg]);
         if(inFile.fail()){
-            std::cout << argv[arg] << ": " << -1 << '\n';
+            std::cout << " " << argv[arg] << ": " << -1 << '\n';
         }else{
             int numLines = 0;
             std::string unused;
             while ( std::getline(inFile, unused) )
                 ++numLines;
-            std::cout << argv[arg] << ": " << numLines << '\n' ;
+            std::cout << " " << argv[arg] << ": " << numLines << '\n' ;
         }
     }
     exit(0); // this means that the program executed correctly!
